81b805fc4baec84fd79a63f2f6fe2453ee0b4c27
AnnaMrowca <62843222+AnnaMrowca@users.noreply.github.com>
2020-03-29 17:37:49 +0200

    Add files via upload

---
+++ Zadania_27.03.2020.py
@@ -0,0 +1,41 @@
+#Zadanie 1
+
+if __name__=='__main__':
+    for i in range (0, 10):
+        print('Anna')
+
+
+#Zadanie 2
+if __name__=='__main__':
+    i = 7
+    wynik = i * 3
+    print('i pomno¿one przez 3 = ' + str(wynik))
+
+
+#Zadanie 3
+if __name__=='__main__':
+    i = 60
+    if 0 <= i <= 10:
+        print(' jesteœ dzieckiem')
+    elif 11 <= i <= 20:
+        print('jesteœ nastolatkiem')
+    elif i >= 21 <= 60:
+        print('jesteœ doros³y')
+    else:
+        print('jesteœ emerytem')
+
+
+#Zadanie 4
+
+if __name__=='__main__':
+    i = 63
+    if i % 3 == 0:
+        print('fizz')
+    elif i % 5 == 0:
+        print('buzz')
+    else:
+        print(str(i))
+
+
+
+
