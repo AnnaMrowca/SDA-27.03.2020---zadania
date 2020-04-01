839df2a5127efa67ddb2b5721c14a2b228748d0d
AnnaMrowca <62843222+AnnaMrowca@users.noreply.github.com>
2020-04-01 09:09:45 +0200

    Add files via upload

---
+++ Zad 1 i 4 - poprawki_01.04.2020.py
@@ -0,0 +1,22 @@
+Zadanie 2:
+if __name__=='__main__':
+
+    for i in range(1, 10):
+        print(i * 3)
+
+wyœwietla ca³¹ tabliczkê mno¿enia dla zakresu
+
+Zadanie 4:
+
+if __name__=='__main__':
+    i =45
+    if i % 3 == 0 and i % 5 == 0:
+        print('fizzbuzz')
+    elif i % 3 == 0:
+        print('fizz')
+    elif i % 5 == 0:
+        print('buzz')
+    else:
+        print(str(i))
+
+Doda³am zakres: podzielne przez 3 i podzielne przez 5
